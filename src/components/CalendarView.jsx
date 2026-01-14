718d0ce580bec98ed469eac8b64942bc76497591
Wurk <work.email.bruh@gmail.com>
2026-01-14 04:31:58 -0500

    Add 11 PM time slot to calendar view for full 12 hours

--- src/components/CalendarView.jsx
+++ src/components/CalendarView.jsx
@@ -5,7 +5,7 @@ import { format, startOfWeek, addDays, isToday as isTodayFn } from 'date-fns';
 const TIME_SLOTS = [
   '6 AM', '7 AM', '8 AM', '9 AM', '10 AM', '11 AM',
   '12 PM', '1 PM', '2 PM', '3 PM', '4 PM', '5 PM',
-  '6 PM', '7 PM', '8 PM', '9 PM', '10 PM'
+  '6 PM', '7 PM', '8 PM', '9 PM', '10 PM', '11 PM'
 ];
 
 // Height per hour in pixels
